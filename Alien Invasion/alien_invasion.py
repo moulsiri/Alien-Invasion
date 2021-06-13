6e170800f55690d5c783157884d993dec062b9b2
Moulsiri Awasthi <67172708+moulsiri@users.noreply.github.com>
2021-06-13 08:48:38 +0530

    ship moves
    In my game, space ship can move left and right

--- Alien Invasion/alien_invasion.py
+++ Alien Invasion/alien_invasion.py
@@ -1,22 +1,25 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 def run_game():
     #initialize game and create a screen object
     pygame.init()
-    screen=pygame.display.set_mode((1200,700))
+    ai_settings=Settings()
+    screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     #setting bsckground color
-    
-
+    #Make a ship
+    ship=Ship(screen)
     #start the main loop for the game
     while True:
-        #watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                sys.exit()
+        
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
 
-        #make the most recently drawn screen visible
-        pygame.display.flip()
+        
 
 run_game()
 
